1ec0bfe377f3017e46c414e235f42247b2665449
Stepan Sert <sertstepka@gmail.com>
2026-04-28 03:14:37 +0300

    Use EnsureCreated for production fix

--- backend/CasaDiFratelli.Api/Program.cs
+++ backend/CasaDiFratelli.Api/Program.cs
@@ -24,7 +24,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 }
 
 app.UseCors("AllowFrontend");
